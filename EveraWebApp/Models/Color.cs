3b2c9fc43f18fc33b783f9e124b8bb7ada15af74
FidanNovruzlu <fidan.novruzlu0309@gmail.com>
2023-05-02 10:54:53 +0300

    initial commmit

---
+++ EveraWebApp/Models/Color.cs
@@ -0,0 +1,9 @@
+﻿namespace EveraWebApp.Models
+{
+    public class Color
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ EveraWebApp/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace EveraWebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CatagoryId { get; set;}
+        public Catagory Catagory { get; set; }
+        public List<Color> Colors { get; set;}
+    }
+}
