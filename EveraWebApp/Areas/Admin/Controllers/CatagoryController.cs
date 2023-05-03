fcd44cd9df4b1ac9a9975b42434c031b99613b05
FidanNovruzlu <fidan.novruzlu0309@gmail.com>
2023-05-03 08:45:35 +0300

    Initial commit

---
+++ EveraWebApp/Areas/Admin/Controllers/CatagoryController.cs
@@ -0,0 +1,31 @@
+﻿using EveraWebApp.DataContext;
+using EveraWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EveraWebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CatagoryController : Controller
+    {
+        private readonly EveraDbContext _everaDbContext;
+        public CatagoryController (EveraDbContext everaDbContext)
+        {
+            _everaDbContext=everaDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Catagory> catagories = await _everaDbContext.Catagories.ToListAsync();
+            return View(catagories);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            Catagory? catagory=await _everaDbContext.Catagories.FindAsync(id);
+            if (catagory == null)
+            {
+                return NotFound();
+            }
+            return View(catagory);
+        }
+    }
+}
